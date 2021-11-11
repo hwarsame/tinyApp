f8491ea5485347bb94f61da9f0159de8fc3653e7
Hamza Warsame <hamza360123@gmail.com>
2021-11-10 19:01:16 -0500

    Complete URL shortening for tinyApp

--- express_server.js
+++ express_server.js
@@ -2,23 +2,54 @@ const { request } = require("express");
 const express = require("express");
 const app = express();
 const PORT = 8000; // default port 8000
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 app.set('view engine', 'ejs');
+
 
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+function generateRandomString() {
+  let string = '';
+  const chars = '1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+  for (i = 0; i <= 6; i++) {
+    string += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return string;
+}
 
 app.get('/urls', (req, res) => {
-  const templateVars = {urls: urlDatabase};
+  const templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
 });
 
-app.get('/url/:shortURL', (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+app.post("/urls", (req, res) => {
+  let longUrl = req.body.longURL;
+  let shortUrl = generateRandomString();
+  //Adds both urls to database, while shortURL being the random generated one
+  urlDatabase[shortUrl] = longUrl;
+  const templateVars = {shortURL  :shortUrl, longURL :longUrl};
   res.render('urls_show', templateVars);
+  // res.redirect('/url/:shortURL');
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.get('/url/:shortURL', (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render('urls_show', templateVars);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
 });
 
 app.get("/", (req, res) => {
@@ -30,17 +61,17 @@ app.get('/urls.json', (req, res) => {
 });
 
 app.get('/hello', (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n")
-})
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
 
 app.get("/set", (req, res) => {
   const a = 1;
   res.send(`a = ${a}`);
- });
- 
- app.get("/fetch", (req, res) => {
+});
+
+app.get("/fetch", (req, res) => {
   res.send(`a = ${a}`);
- });
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
