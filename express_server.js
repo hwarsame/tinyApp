1b4d943f8ba266d5cd54186f7a6b0f49025a8b9d
Hamza Warsame <hamza360123@gmail.com>
2021-11-14 17:04:13 -0500

    Implement cookie sessions to encrypt cookies

--- express_server.js
+++ express_server.js
@@ -3,14 +3,17 @@ const express = require("express");
 const app = express();
 const PORT = 8000; // default port 8000
 const bodyParser = require("body-parser");
-const cookieParser = require('cookie-parser');
+// const cookieParser = require('cookie-parser');
 const cookieSession = require('cookie-session');
 const bcrypt = require('bcryptjs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(cookieParser());
-// app.use(cookieSession());
+
+ app.use(cookieSession({
+   name: 'session',
+   keys: ['cookiesEncrypt']
+ }));
 
 app.set('view engine', 'ejs');
 
@@ -69,22 +72,26 @@ function generateRandomString() {
 }
 
 app.get('/urls', (req, res) => {
-  const user_id = req.cookies.user_id
+  const user_id = req.session.user_id
   if (!user_id){
     res.status(401).send('You must be logged in to view the urls')
   }
+  const userUrls = urlsForUser(user_id, urlDatabase)
   // const user = users[user_id];;
-  const templateVars = { urls: urlDatabase, user: req.cookies.user_id};
-  // const templateVars = { urls: urlsForUser(user_id.id, urlDatabase), user: req.cookies.user_id};
+  const templateVars = { urls: userUrls, user: users[user_id]};
   res.render('urls_index', templateVars);
 });
 
 app.post("/urls", (req, res) => {
-  let longURL = req.body.longURL;
+  let longUrl = req.body.longURL;
   let shortUrl = generateRandomString();
   //Adds both urls to database, while shortURL being the random generated one
-  urlDatabase[shortUrl] = {longURL, userID: req.cookies.user_id.id};
-  const templateVars = {shortURL: shortUrl, longURL: urlDatabase[shortUrl].longURL, user: req.cookies.user_id};
+  urlDatabase[shortUrl] = {
+    longURL: longUrl, 
+    userID: req.session.user_id
+  };
+
+  const templateVars = {shortURL: shortUrl, longURL: longUrl, user: users[req.session.user_id]};
   res.render('urls_show', templateVars);
   // res.redirect('/url/:shortURL');
 });
@@ -107,7 +114,8 @@ app.post('/register', (req, res) => {
     };
     // hashedPass = bcrypt.hashSync(req.body.password, 10);
     users[id] = user;
-    res.cookie("user_id", {id: id, email: email});
+    // res.cookie("user_id", {id: id, email: email});
+    req.session.user_id = id
     res.redirect("/urls");
       
   }
@@ -116,31 +124,31 @@ app.post('/register', (req, res) => {
 
 app.get("/urls/new", (req, res) => {
   const templateVars = {user: null}
-  let user = req.cookies.user_id
+  let user = req.session.user_id
   if (!user){
     return res.redirect('/login')
   } 
-    templateVars.user = user
+    templateVars.user = users[user];
     res.render("urls_new", templateVars);
     return;
 });
 //Get shortURL
 app.get('/urls/:shortURL', (req, res) => {
-  const user = req.cookies.user_id 
+  const user = req.session.user_id 
   if (!user){
     res.status(401).send('You must be logged in to access this page.')
   }
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL].longURL, user: req.cookies.user_id};
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL].longURL, user: users[user]};
   res.render('urls_show', templateVars);
 });
 
 //Delete button
 app.post(`/urls/:shortURL/delete`, (req, res) => {
-  const user = req.cookies.user_id
+  const user = req.session.user_id
   if (!user){
     return res.status(401).send('You must be logged in to access this feature');
   }
-  const userUrls = urlsForUser(user.id, urlDatabase)
+  const userUrls = urlsForUser(user, urlDatabase)
   let shortURL = req.params.shortURL;
   if (!Object.keys(userUrls).includes(shortURL)){
     return res.status(401).send('You are not permitted to delete this URL')
@@ -152,11 +160,11 @@ app.post(`/urls/:shortURL/delete`, (req, res) => {
   
 //Edit button
 app.post('/urls/:shortURL', (req, res) => {
-  const user_id = req.cookies.user_id
+  const user_id = req.session.user_id
   if (!user_id) {
     return res.status(401).send('You must be logged in to access this feature')
   }
-  const userUrls = urlsForUser(user_id.id, urlDatabase)
+  const userUrls = urlsForUser(user_id, urlDatabase)
   let shortURL = req.params.shortURL;
   if (!Object.keys(userUrls).includes(shortURL)){
     return res.status(401).send('You are not permitted to edit this URL')
@@ -173,7 +181,7 @@ app.get("/u/:shortURL", (req, res) => {
 
 //Log out of TinyApp
 app.post('/logout', (req, res) => {
-  res.clearCookie('user_id');
+  req.session = null;
   res.redirect('/urls');
 });
 
@@ -192,7 +200,7 @@ app.post('/login', (req, res) => {
   if (user) {
     console.log('USER DATA#####', user);
     if (bcrypt.compareSync (password , user.password)) {
-      res.cookie('user_id', user)
+      req.session.user_id = user.id
       res.redirect('/urls')
       console.log('LOG IN PASSWORD >>>>>', user.password)
     } else {
@@ -203,11 +211,11 @@ app.post('/login', (req, res) => {
   }
 });
 
-//Logout button
-app.get('/logout', (req, res)=> {
-  res.clearCookie('user_id');
-  res.redirect('/urls');
-});
+// //Logout button
+// app.get('/logout', (req, res)=> {
+//   // req.session = null
+//   res.redirect('/urls');
+// });
 
 
 
@@ -219,8 +227,11 @@ app.post('/register', (req, res) => {
 
 //Register an account page should show up
 app.get('/register', (req, res) => {
+  // const userId = req.session.user_id
+  // if(userId){
+  //   res.redirect('/urls')
+  // }
   res.render('urls_registration',{user: null});
-  
 });
 
 app.get("/", (req, res) => {
